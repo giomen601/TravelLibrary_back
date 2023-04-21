ac510ddcd845d4834a30abbb632760845c713460
giomen601 <arizamendozagiovany@gmail.com>
2023-04-20 22:49:33 -0500

    Auth-initial-update

---
+++ src/Core/Travel.Library.Application/Models/Identity/AuthResponse.cs
@@ -0,0 +1,8 @@
+namespace Travel.Library.Application.Models.Identity;
+public class AuthResponse
+{
+  public string? Id { get; set; }
+  public string? UserName { get; set; }
+  public string? Email { get; set; }
+  public string? Token { get; set; }
+}
---
+++ src/Core/Travel.Library.Application/Models/Identity/RegistrationResponse.cs
@@ -0,0 +1,5 @@
+namespace Travel.Library.Application.Models.Identity;
+public class RegistrationResponse
+{
+  public string? UserId { get; set; }
+}
